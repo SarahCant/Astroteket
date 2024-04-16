13bb5e2fddd0d07aab8bb8be76377bc8cfbe81a0
Inger-Margrethe <imhp@hotmail.dk>
2024-04-16 13:56:08 +0200

    startup html

---
+++ script.js
@@ -0,0 +1,11 @@
+//scene 1 - tekst-array
+
+const sc1Txt = [
+    "Når vi kigger op på en skyfri nattehimmel,",
+    "kan vi se et par tusinde stjerner.",
+    "Men i virkeligheden findes der mange milliarder.",
+    "De er lavet i enorme gaståger,",
+    "og tager mange millioner år om at udvikle sig.",
+    "Hjælp mig med at følge opskriften og se,",
+    "hvordan stjerner bliver skabt og udvikler sig!"
+];
