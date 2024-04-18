768597248a6dff02d7f74c3cf8d3c245cca5affa
Inger-Margrethe <imhp@hotmail.dk>
2024-04-18 13:54:45 +0200

    end of day javascript

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ billeder.forEach(function(img) {
 });
 
 
-//SCENE 1 - tekst-array
+//SCENE 1
 const sc1Txt = [
     //scene1.1
-    "Når vi kigger op på en skyfri nattehimmel,",
+    //"Når vi kigger op på en skyfri nattehimmel,",
     "kan vi se et par tusinde stjerner.",
     //scene1.2
     "Men i virkeligheden findes der mange milliarder.",
@@ -23,10 +23,43 @@ const sc1Txt = [
     "hvordan stjerner bliver skabt og udvikler sig!"
 ];
 
-//SCENE 2 - tekst-array
+const sc11 = document.querySelector(".scene1_1");
+const sc12 = document.querySelector(".scene1_2");
+const sc13 = document.querySelector(".scene1_3");
+
+function sc1TxtSkift() {
+    const plads = document.getElementById("txt1");
+    let visTxt = 0;
+
+    const interval = setInterval(() => {
+        plads.textContent = sc1Txt[visTxt];
+        visTxt++;
+
+        if (visTxt === 2) {
+            sc11.style.display = "none";
+            sc12.style.display = "grid";
+            sc13.style.display = "none";
+        }
+
+        if (visTxt === 5) {
+            sc11.style.display = "none";
+            sc12.style.display = "none";
+            sc13.style.display = "grid";
+        }
+
+        if (visTxt >= sc1Txt.length) {
+            clearInterval(interval);
+        }
+
+    }, 3000);
+}
+
+sc1TxtSkift();
+
+//SCENE 2
 const sc2Txt = [
     //scene2.1
-    "Når du har fundet ingredienserne,",
+    //"Når du har fundet ingredienserne,",
     "kommer de i kolben.",
     //scene2.2
     "Her er det, du skal finde:",
@@ -36,25 +69,59 @@ const sc2Txt = [
     //scene2.3
     "Kan du se ingredienserne?",
     "Prøv at tryk på dem, vi skal bruge.",
-    //scene2.4
-    "Godt klaret!",
-    "Du har fundet alle stjernernes ingredienser.",
 ];
 
-//SCENE 3 - tekst-array
+const sc21 = document.querySelector(".scene2_1");
+const sc22 = document.querySelector(".scene2_2");
+const sc23 = document.querySelector(".scene2_3");
+
+function sc2TxtSkift() {
+    const plads = document.getElementById("txt2");
+    let visTxt = 0;
+
+    const interval = setInterval(() => {
+        plads.textContent = sc2Txt[visTxt];
+        visTxt++;
+
+        if (visTxt === 2) {
+            sc21.style.display = "none";
+            sc22.style.display = "grid";
+            sc23.style.display = "none";
+        }
+
+        if (visTxt === 6) {
+            sc21.style.display = "none";
+            sc22.style.display = "none";
+            sc23.style.display = "grid";
+        }
+
+        if (visTxt >= sc2Txt.length) {
+            clearInterval(interval);
+        }
+
+    }, 2500);
+}
+
+sc2TxtSkift();
+
+
+//SCENE 3
 const sc3Txt = [
+    //scene2.4
+    //"Godt klaret!",
+    "Du har fundet alle stjernernes ingredienser.",
     //scene3.1
     "Blandingen skal over i en skål.",
-    "Skålen er rigtig stor og har en diameter på...",
-    "...28 millioner milliarder meter.",
+    "Skålen er rigtig stor og har en diameter på",
+    "28 millioner milliarder meter.",
     //scene3.2
     "Så skal vi røre det hele godt sammen.",
     //scene3.3
     "Blandingen har brug for at stå et koldt sted.",
     "Helst så koldt som muligt. -263℃ er helt perfekt!",
     //scene3.4
-    "Nu lader vi stjerne-blandingen hvile i ",
-    "mellem 100.000 og 10 millioner år.",
+    "Nu lader vi stjerne-blandingen hvile",
+    "imellem 100.000 og 10 millioner år.",
     //scene3.5
     "Så er stjernerne klar",
     "og vi har nu en åben stjernehob!",
@@ -62,19 +129,113 @@ const sc3Txt = [
     "og temperatur."
 ];
 
-//SCENE 4 - tekst-array
+const sc24 = document.querySelector(".scene2_4");
+const sc31 = document.querySelector(".scene3_1");
+const sc32 = document.querySelector(".scene3_2");
+const sc33 = document.querySelector(".scene3_3");
+const sc34 = document.querySelector(".scene3_4");
+const sc35 = document.querySelector(".scene3_5");
+
+function sc3TxtSkift() {
+    const plads = document.getElementById("txt3");
+    let visTxt = 0;
+
+    const interval = setInterval(() => {
+        plads.textContent = sc3Txt[visTxt];
+        visTxt++;
+
+        if (visTxt === 2) {
+            sc24.style.display = "none";
+            sc31.style.display = "grid";
+            sc32.style.display = "none";
+            sc33.style.display = "none";
+            sc34.style.display = "none";
+            sc35.style.display = "none";
+        }
+
+        if (visTxt === 5) {
+            sc24.style.display = "none";
+            sc31.style.display = "none";
+            sc32.style.display = "grid";
+            sc33.style.display = "none";
+            sc34.style.display = "none";
+            sc35.style.display = "none";
+        }
+
+        if (visTxt === 6) {
+            sc24.style.display = "none";
+            sc31.style.display = "none";
+            sc32.style.display = "none";
+            sc33.style.display = "grid";
+            sc34.style.display = "none";
+            sc35.style.display = "none";
+        }
+
+        if (visTxt === 8) {
+            sc24.style.display = "none";
+            sc31.style.display = "none";
+            sc32.style.display = "none";
+            sc33.style.display = "none";
+            sc34.style.display = "grid";
+            sc35.style.display = "none";
+        }
+
+        if (visTxt === 10) {
+            sc24.style.display = "none";
+            sc31.style.display = "none";
+            sc32.style.display = "none";
+            sc33.style.display = "none";
+            sc34.style.display = "none";
+            sc35.style.display = "grid";
+        }
+
+        if (visTxt >= sc3Txt.length) {
+            clearInterval(interval);
+        }
+
+    }, 3000);
+}
+
+sc3TxtSkift();
+
+//SCENE 4
 const sc4Txt = [
     //scene4.1
-    "Der kan nu ske to forskellige ting;",
+    //"Der kan nu ske to forskellige ting;",
     "Planetarisk tåge eller Supernova.", //tekst-alternativ: fremtiden for den varme røde stjerne eller fremtiden for den endnu varmere, blå stjerne...
     //scene4.2
     "Vælg den, du helst vil se." //tekst-alternativ: Vælg en stjerne, og se hvad der sker.
 ];
 
+const sc41 = document.querySelector(".scene4_1");
+const sc42 = document.querySelector(".scene4_2");
+
+function sc4TxtSkift() {
+    const plads = document.getElementById("txt4");
+    let visTxt = 0;
+
+    const interval = setInterval(() => {
+        plads.textContent = sc4Txt[visTxt];
+        visTxt++;
+
+        if (visTxt === 2) {
+            sc41.style.display = "none";
+            sc42.style.display = "grid";
+        }
+
+        if (visTxt >= sc4Txt.length) {
+            clearInterval(interval);
+        }
+
+    }, 3000);
+}
+
+sc4TxtSkift();
+
 //SCENE 5A - tekst-array
 const sc5aTxt = [
     //scene5a.1
-    "Stjerneblandingen har bedst af at stå i",
+    //"Stjerneblandingen har bedst af at stå i",
     "nogle milliarder år mere.",
     //scene5a.2
     "Til sidst vil de små, lette stjerner også begynde",
@@ -83,6 +244,31 @@ const sc5aTxt = [
     "så der til sidst kun er deres kerner tilbage.",
     "Det er nu en Hvid Dværg."
 ];
+
+const sc51 = document.querySelector(".scene5_1");
+const sc5a2 = document.querySelector(".scene5a_2");
+
+function sc4TxtSkift() {
+    const plads = document.getElementById("txt4");
+    let visTxt = 0;
+
+    const interval = setInterval(() => {
+        plads.textContent = sc4Txt[visTxt];
+        visTxt++;
+
+        if (visTxt === 2) {
+            sc41.style.display = "none";
+            sc42.style.display = "grid";
+        }
+
+        if (visTxt >= sc4Txt.length) {
+            clearInterval(interval);
+        }
+
+    }, 3000);
+}
+
+sc4TxtSkift();
 
 //SCENE 5B - tekst-array
 const sc5bTxt = [
@@ -100,3 +286,5 @@ const sc5bTxt = [
     "se sorte hullers tyngdekraft er så stærk, at",
     "de opsluger alt omkring til sig - selv lys!"
 ];
+
+// document.getElementById("txt5b").innerHTML = sc5bTxt;
